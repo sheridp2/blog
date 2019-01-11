dbc168bbcef9c6efe0af51e0b4979208381113e3
Patrick Sheriddan <sheridp2@gmail.com>
2019-01-10 18:56:13 -0800

    cors fix, hopefully

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require('cors');
 const keys = require("./config/keys");
 require("./models/Blogs");
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
-
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 require("./routes/blogRoutes")(app);
 
